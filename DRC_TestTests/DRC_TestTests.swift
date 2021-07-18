279177fd0bc86a3ed3e9388c1b9365c20dab9d4f
nihalk <nihalk@softcell.com>
2021-07-18 15:16:53 +0530

    Initial Commit

---
+++ DRC_TestTests/DRC_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  DRC_TestTests.swift
+//  DRC_TestTests
+//
+//  Created by Nihal Khokhari on 18/07/21.
+//
+
+import XCTest
+@testable import DRC_Test
+
+class DRC_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
